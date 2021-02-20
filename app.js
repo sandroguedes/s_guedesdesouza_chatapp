82e8009375e04bd6e1576e39e34cf47a194edffc
sandroguedes <59712770+sandroguedes@users.noreply.github.com>
2021-02-19 19:36:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const messenger = require("socket.io")();
+const app = express();
+const port = process.env.PORT || 5050;
+const server = app.listen(port, () => {
+    console.log(`app currently executing on ${port}`);
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+messenger.attach(server);
+
+messenger.on('connection', (socket) => {
+    console.log(`1 user CONNECTED: ${socket.id}`);
+    socket.emit('CONNECTED', { socketID: `${socket.id}`, message: 'a new CONNECTION has been detected' });
+    socket.on('ChatMESSAGE', function(message) {
+        console.log(message);
+        messenger.emit('MESSAGE', { id: socket.id, message: message })
+    });
+    socket.on("disconnect", () => {
+        console.log("1 USER has just DISCONNECTED");
+    })
+});
