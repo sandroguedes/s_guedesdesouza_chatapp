6e7bb76a9324dd3bf75a31ba8656a8905fa9a29d
sandroguedes <59712770+sandroguedes@users.noreply.github.com>
2021-02-19 20:20:07 -0500

    Add files via upload

---
+++ public/scripts/main.js
@@ -0,0 +1,45 @@
+import ChatMessage from "./components/TheMessageComponent.js"
+
+(() => {
+    console.log('George RR Martin will finish freaking WINDS OF WINTER before I manage to properly debug this thing...');   
+    
+    const socket = io();
+
+    function setUserId({ socketID, message }) {
+        vm.socket_ID = socketID;
+    }
+
+    function appendMessage(message) {
+        console.log(vm);
+        vm.messages.push(message);;
+        vm.$refs.notification_sound.play();
+    }
+
+    const vm = new Vue({
+        data: {
+            messages: [],
+            nickname: "",
+            socket_ID: "",
+            message: "",
+            inTheChat: false
+        },
+        create: function() {
+            console.log("IT's ON!");
+        },
+        methods: {
+            joinTheChat() {
+                this.inTheChat = true;
+                vm.$refs.boot_sound.play();
+            },
+            dispatchMessage() {
+                socket.emit('ChatMESSAGE', { content: this.message, name: this.nickname || "Anonymous" });
+            }
+        },
+        components: {
+            newmessage: ChatMessage
+        }
+    }).$mount("#app");
+
+    socket.addEventListener("connected", setUserId);
+    socket.addEventListener("message", appendMessage);
+})();
